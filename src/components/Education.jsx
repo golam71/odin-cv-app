fcaf47931697469e1d85150e7dfb9e2af3a9094d
Golam <74001687+golam71@users.noreply.github.com>
2025-10-03 20:34:52 +0600

    ♻️ Minor refactor

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,60 +1,27 @@
 import { useState } from "react";
 
 export function Education() {
-  const [isEditing, setIsEditing] = useState(true);
-  const [EducationData, setEducationData] = useState({
-    "Instuition Name": "",
+  let data = {
+    Name: "",
     Degree: "",
     "Start Date": "",
     "End Date": "",
     Location: "",
-  });
+  };
 
-  function handleChange(e) {
-    setEducationData({ ...EducationData, [e.target.name]: e.target.value });
-  }
-
-  function Inputs() {
-    let data = [
-      "Instuition Name",
-      "Degree",
-      "Start Date",
-      "End Date",
-      "Location",
-    ];
-
-    return data.map((d) => (
-      <label htmlFor={d} key={d}>
-        <br />
-        {d}
-        <br />
-        <input type="text" name={d} onChange={(e) => handleChange(e)} />
-      </label>
-    ));
-  }
+  const [eduData, setEduData] = useState([data]);
 
   return (
     <div className="education">
       <h2>Education</h2>
-      {!isEditing && (
-        <button
-          onClick={() => {
-            setIsEditing(!isEditing);
-          }}
-        >
-          + Add
-        </button>
-      )}
-      {isEditing && <Inputs />}
-      {isEditing && (
-        <button
-          onClick={() => {
-            setIsEditing(!isEditing);
-          }}
-        >
-          Sumbit
-        </button>
-      )}
+      {Object.keys(data).map((element) => {
+        return (
+          <label htmlFor={element} key={element}>
+            {element}
+            <input type="text" id={element} />
+          </label>
+        );
+      })}
     </div>
   );
 }
