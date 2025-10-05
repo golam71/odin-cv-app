4a64666aa36211e37719ca33f4cb9b2841758fb6
Golam <74001687+golam71@users.noreply.github.com>
2025-10-05 23:59:39 +0600

    ✨ Implement CV component structure and add initial CSS styles for layout and design

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,7 +1,45 @@
-export default function CV() {
+import "../css/cv.css";
+
+export default function CV({ personalDetails, eduData, expData }) {
   return (
     <div className="cv">
-      <p>CV</p>
+      <Personal personalDetails={personalDetails} />
+      <div className="edu">
+        <h2>Education</h2>
+        <Education eduData={eduData} />
+      </div>
     </div>
   );
+}
+
+function Personal({ personalDetails }) {
+  return (
+    <div className="personal">
+      <h1>{personalDetails["Full Name"]}</h1>
+      <div className="flex">
+        <p>{personalDetails["Email"]}</p>
+        <p>{personalDetails["Phone Number"]}</p>
+        <p>{personalDetails["Address"]}</p>
+      </div>
+    </div>
+  );
+}
+
+function Education({ eduData }) {
+  return eduData.map((item) => {
+    return (
+      <div className="between">
+        <div>
+          <p>
+            {item["Start Time"]} - {item["End Time"]}
+          </p>
+          <p>{item["Location"]}</p>
+        </div>
+        <div>
+          <b>{item["Name"]}</b>
+          <p>{item["Degree"]}</p>
+        </div>
+      </div>
+    );
+  });
 }
